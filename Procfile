6216e474427e279ccfa14179d41f784773b994bc
socafofin <roberth.jb2021@gmail.com>
2025-02-19 06:49:13 -0300

    teste32131

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: python server.py
 release: flask db upgrade
+web: gunicorn --bind 0.0.0.0:$PORT server:app
--- server.py
+++ server.py
@@ -135,4 +135,3 @@ def ping():
 if __name__ == '__main__':
     # Verifique se a variável de ambiente FLASK_ENV está definida como 'development'
     if os.environ.get("FLASK_ENV") == "development":
-        app.run(host='0.0.0.0', port=5000, debug=True)  # Modo de desenvolvimento
